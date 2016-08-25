b633ec8fc2bb94c5976fd4e29dfd4190fb80fb4f
William Naughton-Gravette <william@willng.me>
2016-08-25 22:58:55 +1000

    more homepage mockup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,17 @@
 @endsection
 @section('body')
 <div class="pure-g">
-    <div class="pure-u-14-24">
+    <div class="pure-u-1 pure-u-lg-14-24 intro">
         <div class="l-box">
             <h1><strong>Naughton & Ross</strong> is a design & development studio based in <strong>Melbourne, Australia</strong>.</h1>
             <h1>Focusing our work on <strong>Web Design & Development</strong>, we can also help with graphic design, print and publishing design, stationery, and photography.</h1>
+            <p>
+                <strong>See More &#8212;</strong> <a href="">Blog</a> <span>&middot;</span> <a href="">About our Business</a> <span>&middot;</span> <a href="">Get in Touch</a>
+            </p>
         </div>
     </div>
-    <div class="pure-u-2-24"></div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-1 pure-u-lg-2-24"></div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square first text">
             <div class="l-box">
                 <p>
@@ -24,7 +27,7 @@
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square clear">
 
         </div>
@@ -34,48 +37,110 @@
     </div>
 </div>
 <div class="pure-g">
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square img biglife">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square img rubix">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square clear">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square img pwps">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square colour red text">
+            <div class="l-box">
+                <p>
+                    News <span>&middot;</span> Design &#8212;
+                </p>
+                <h2><i>Preston West Primary</i> gets a makeover</h2>
+            </div>
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square clear">
+
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square img bsh">
+
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square">
+
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square">
+
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square img brs">
+
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square clear">
+
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square">
+
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square img ">
+
+        </div>
+    </div>
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
         <div class="pure-u-1 home_square colour yellow text">
+            <div class="l-box">
+                <p>
+                    Insights <span>&middot;</span> Design &#8212;
+                </p>
+                <h2>Building your brand: the process</h2>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="pure-g">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square second text">
             <div class="l-box">
                 <p>
                     News <span>&middot;</span> Web &#8212;
@@ -83,20 +148,24 @@
                 <h2><i>Little Sports Heroes</i> goes online</h2>
             </div>
         </div>
-    </div>
-    <div class="pure-u-4-24">
         <div class="pure-u-1 home_square clear">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
-        <div class="pure-u-1 home_square img bsh">
+    <div class="pure-u-8-24 pure-u-md-6-24 pure-u-lg-4-24">
+        <div class="pure-u-1 home_square clear">
+
+        </div>
+        <div class="pure-u-1 home_square first">
 
         </div>
     </div>
-    <div class="pure-u-4-24">
-        <div class="pure-u-1 home_square">
-
+    <div class="pure-u-2-24"></div>
+    <div class="pure-u-1 pure-u-lg-14-24">
+        <div class="l-box">
+            <h1><strong>This is <i>just some of</i> our portfolio.</strong> Have a look around.</h1>
+            <h1>We cater for <strong>projects of every kinds and size</strong>. We love working with small local businesses as much as we do with school and larger businesses too.</h1>
+            <h1>If you like what you see, feel free to <a href="">get in touch</a>.</h1>
         </div>
     </div>
 </div>
