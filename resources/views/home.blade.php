516ea12ec9f739dbfa0da92d868070b60a264441
William Naughton-Gravette <william@willng.me>
2016-08-25 15:14:43 +1000

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('template')
+@section('head')
+<link rel="stylesheet" href="{{asset('css/home.css')}}" />
+@endsection
+@section('body')
+<div class="pure-g">
+    <div class="pure-u-14-24">
+        <div class="l-box">
+            <h1><strong>Naughton & Ross</strong> is a design & development studio based in <strong>Melbourne, Australia</strong>.</h1>
+            <h1>Focusing our work on <strong>Web Design & Development</strong>, we can also help with graphic design, print and publishing design, stationery, and photography.</h1>
+        </div>
+    </div>
+    <div class="pure-u-5-24">
+
+    </div>
+    <div class="pure-u-5-24">
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NR2017</title>
+        <script src="https://use.typekit.net/rnd5fkg.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+        <!--[if lte IE 8]>
+            <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+            <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+        <!--<![endif]-->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}" />
+        @yield('head')
+    </head>
+    <body>
+        <div id="master_wrapper">
+            @yield('body')
+        </div>
+    </body>
+</html>
