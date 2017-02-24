090f41133e019a094fe763a52112f0f7e2039e38
William‌‌ Naughton-Gravette <william@willng.me>
2017-02-24 19:33:38 +1100

    Fix css

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,12 +6,12 @@
         <script src="https://use.typekit.net/rnd5fkg.js"></script>
         <script>try{Typekit.load({ async: true });}catch(e){}</script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pure/0.6.2/pure-min.css">
         <!--[if lte IE 8]>
-            <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pure/0.6.2/grids-responsive-old-ie-min.css">
         <![endif]-->
         <!--[if gt IE 8]><!-->
-            <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pure/0.6.2/bgrids-responsive-min.css">
         <!--<![endif]-->
         <link rel="stylesheet" href="{{asset('css/app.css')}}" />
         <link rel="stylesheet" href="{{asset('font-awesome/css/font-awesome.min.css')}}" />
